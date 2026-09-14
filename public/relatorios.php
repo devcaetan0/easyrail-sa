c4ff34de3f0e9859f63a6369b8703f817571dbbd
gabrielribeiro-code <gabriel_ribeiro16@estudante.sesisenai.org.br>
2026-09-14 09:06:42 -0300

    feat - arrumando algumas alterações na página de relatorios e arrumando estrutura da pagina

--- public/relatorios.php
+++ public/relatorios.php
@@ -11,26 +11,26 @@ include "../infra/conexao.php";
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="icon" href="../assets/images/logo.png" type="image/png">
-    <title>Sensores</title>
+    <title>Histórico Analítico</title>
 </head>
 
 <body>
     <?php include('../components/navbar.html') ?>
 
-    <h1 class="titulo-pagina fw-bold">Relatórios</h1>
+    <h1 class="titulo-pagina fw-bold">Histórico Analítico</h1>
 
     <main class="main-padrao">
         <div class="container-fluid">
-            <div class="d-flex justify-content-end mb-3">
+            <div class="d-flex justify-content-end mb-2">
             </div>
 
             <div class="card p-3 mb-4 shadow-sm">
                 <div class="row">
-                    <div class="col-md-6 mb-2">
+                    <div class="col-md-4 mb-2">
                         <label class="fw-bold form-label">Pesquisar</label>
                         <input class="form-control" placeholder="Modelo/ID">
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-4 mb-2">
                         <label class="fw-bold form-label">Falha</label>
                         <select class="form-select">
                             <option>Todas</option>
@@ -38,8 +38,9 @@ include "../infra/conexao.php";
                             <option>Combustível</option>
                             <option>Peso</option>
                         </select>
-                        <div/>
-                  <div class="col-md-6 mb-2">
+                </div>
+
+                  <div class="col-md-4 mb-2">
                     <label class="fw-bold form-label">ID do trem</label>
                         <input class="form-control" placeholder="ID">
                     </div>
@@ -66,8 +67,8 @@ include "../infra/conexao.php";
                                 <td id="1" class="tipo-tr">Alto</td>
                                 <td>
                                     <button class="editar btn btn-sm btn-outline-secondary" data-bs-toggle="modal"
-                                        data-bs-target="#modalCadastro" id="1">✏</button>
-                                    <button class="excluir btn btn-sm btn-outline-danger" id="1">🗑</button>
+                                        data-bs-target="#modalCadastro" id="1">👀</button>
+                                    <button class="excluir btn btn-sm btn-outline-danger" id="1">⬇️</button>
                                 </td>
                             </tr>
                         </tbody>
@@ -76,46 +77,8 @@ include "../infra/conexao.php";
             </div>
         </div>
 
-        <div class="modal fade" id="modalCadastro" tabindex="-1" aria-labelledby="modalCadastroLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content text-start">
-                    <div class="modal-header">
-                        <h5 class="modal-title fw-bold" id="modalCadastroLabel">Cadastrar Novo Sensor</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form id="form-sensor">
-                            <div class="mb-3">
-                                <label for="id-sensor" class="form-label fw-bold">Nome</label>
-                                <input type="text" class="form-control" id="edit-nome" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="setor-sensor" class="form-label fw-bold">Responsável</label>
-                                <input type="text" class="form-control" id="edit-responsavel" required>
-                            </div>
-                            <div class="mb-3">
-                                <label class="fw-bold form-label">Tipo</label>
-                                <select class="form-select" id="edit-tipo">
-                                    <option>Temperatura</option>
-                                    <option>Velocidade</option>
-                                    <option>Energia</option>
-                                </select>
-                            </div>
-                            <div class="mb-3">
-                                <label for="nome-sensor" class="form-label fw-bold">Localização</label>
-                                <input type="text" class="form-control" id="edit-localizacao" required>
-                            </div>
-                            <button type="button" class="btn btn-secondary fw-bold"
-                                data-bs-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-laranja" id="btn-salvar">Salvar Sensor</button>
-                        </form>
-                    </div>
-                    <div class="modal-footer justify-content-between">
-                    </div>
-                </div>
-            </div>
-        </div>
+      
+                            
 
     </main>
 
